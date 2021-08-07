048d50105477d8c8eeb2d324538e53c09f39d12d
wolvecode <wolvecode@gmail.com>
2021-08-07 22:29:39 +0100

    add student, lecturer and admin migration

--- database/migrations/2021_08_01_093002_create_supervisors_table.php
+++ database/migrations/2021_08_01_093002_create_supervisors_table.php
@@ -16,12 +16,14 @@ class CreateSupervisorsTable extends Migration
         Schema::create('supervisors', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('bio');
+            $table->string('bio')->nullable();
             $table->string('git_url')->nullable();
             $table->string('linkedin_url')->nullable();
+            $table->string('role')->default('Lecturer');
             $table->string('email')->unique();
             $table->string('password');
             $table->boolean('is_head')->default(false);
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2021_08_07_202633_create_students_table.php
+++ database/migrations/2021_08_07_202633_create_students_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAdminsTable extends Migration
+class CreateStudentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,19 @@ class CreateAdminsTable extends Migration
      */
     public function up()
     {
-        Schema::create('admins', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('bio')->nullable();
+            $table->string('email')->nullable();
+            $table->string('bio',1025);
+            $table->foreignId('supervisor_id')->nullable();
+            $table->string('matric_no')->unique()->nullable();
             $table->string('git_url')->nullable();
             $table->string('linkedin_url')->nullable();
-            $table->string('role')->default('Admin');
-            $table->string('password');
-            $table->boolean('is_super')->default(false);
+            $table->string('role')->default('Student');
+            $table->string('location')->nullable();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -36,6 +38,6 @@ class CreateAdminsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('admins');
+        Schema::dropIfExists('students');
     }
 }
