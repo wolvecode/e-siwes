3ac11fe435b2c2b593c40434fe4e6913f572a5e2
wolvecode <wolvecode@gmail.com>
2021-08-07 22:30:39 +0100

    set student, admin and supervisor factory

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Student;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class StudentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Student::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+//            'email' => $this->faker->unique()->safeEmail(),
+            'name'=> 'Adedeji Stephen',
+            'email' => 'students@gmail.com',
+            'matric_no' =>'18D/47CS/00815',
+            'bio' => $this->faker->sentence(),
+            'git_url' => 'www.github.com/wolvecode',
+            'linkedin_url' => 'www.linkedin.com/wolvecode',
+            'password'=> Hash::make('password'), // password
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+        ];
+    }
+}
