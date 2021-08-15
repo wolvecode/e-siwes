06f3b3751ec99d7fb583cdec89d8fec2cc3f4a17
wolvecode <wolvecode@gmail.com>
2021-08-15 13:30:16 +0100

    add organization factory

--- database/factories/OrganizationFactory.php
+++ database/factories/OrganizationFactory.php
@@ -2,17 +2,17 @@
 
 namespace Database\Factories;
 
-use App\Models\Organisation;
+use App\Models\Organization;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class OrganisationFactory extends Factory
+class OrganizationFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = Organisation::class;
+    protected $model = Organization::class;
 
     /**
      * Define the model's default state.
